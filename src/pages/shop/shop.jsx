254be04f40601e641de4e4007d6ebf0350b45392
Sergio Mendez <sergio.mendez.roca@gmail.com>
2023-01-04 12:58:55 -0800

    created shop and cart pages

---
+++ src/pages/shop/shop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Shop = () => {
+   return (
+      <div className='shop'>
+         <div className='shopTitle'>
+          <h1>Sergio's Shop</h1>
+         </div>
+         <div className='products'>
+        
+         </div>
+      </div>
+   )
+}
