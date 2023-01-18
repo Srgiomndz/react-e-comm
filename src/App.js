775a05b0aeef5fd08bdb0de0033536fe87e9c0f5
Sergio Mendez <sergio.mendez.roca@gmail.com>
2023-01-18 11:53:02 -0800

    changed Router basename url to gh page

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
    return (
       <div className='App'>
       <ShopContextProvider>
-         <Router basename={process.env.PUBLIC_URL}>
+         <Router basename='https://github.com/Srgiomndz/react-e-comm'>
          <Navbar />
             <Routes>
                <Route path='/' element={<Shop/>} />
