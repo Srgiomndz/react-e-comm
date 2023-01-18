b31360c44561e18a0a300783d9c99b92a5ed10f1
Sergio Mendez <sergio.mendez.roca@gmail.com>
2023-01-18 12:45:43 -0800

    deployment fix

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
    return (
       <div className='App'>
       <ShopContextProvider>
-         <Router basename='https://github.com/Srgiomndz/react-e-comm'>
+         <Router>
          <Navbar />
             <Routes>
                <Route path='/' element={<Shop/>} />
